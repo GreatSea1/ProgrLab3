2d673a9bb8aaed94965d29c34e0de8f3d9502e6f
GreatSea1 <user.name>
2022-05-27 21:35:52 +0700

    Done Laboratory Work

---
+++ ProgrLab3/MatrixClass.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace ProgrLab3
+{
+
+	public class DeepStructure
+	{
+		public int A { get; set; }
+	}
+
+	class ShallowCloneMatrix : SquareMatrix, ICloneable
+	{
+		public object Clone()
+		{
+			return (SquareMatrix)this.MemberwiseClone();
+		}
+	}
+
+	public class SquareMatrix
+	{
+		private int[,] Matrix;
+		public float Determinant = new Random().Next(1, 10);
+		public DeepStructure DS;
+		public SquareMatrix()
+		{
+			Matrix = new int[3, 3];
+			DS = new DeepStructure();
+		}
+
+		public void FillRandom()
+		{
+			for (int Line = 0; Line < 3; ++Line)
+			{
+				for (int Column = 0; Column < 3; ++Column)
+				{
+					Matrix[Line, Column] = new Random().Next(1, 10);
+					Console.Write("{0} ", Matrix[Line, Column]);
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine("Детерминант: {0}", Determinant);
+			Console.WriteLine();
+		}
+
+		public static float operator /(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix)
+		{
+			return FirstMatrix.Determinant / SecondMatrix.Determinant;
+		}
+		public static float operator *(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix)
+		{
+			return FirstMatrix.Determinant * SecondMatrix.Determinant;
+		}
+		public static float operator +(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix)
+		{
+			return FirstMatrix.Determinant + SecondMatrix.Determinant;
+		}
+
+		public static float operator -(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix)
+		{
+			return FirstMatrix.Determinant - SecondMatrix.Determinant;
+		}
+
+		public static bool operator ==(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix)
+		{
+			return FirstMatrix.Determinant == SecondMatrix.Determinant;
+		}
+
+		public static bool operator !=(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix)
+		{
+			return FirstMatrix.Determinant != SecondMatrix.Determinant;
+		}
+
+		public override bool Equals(object Other)
+		{
+			if (Other == null)
+			{
+				return false;
+			}
+
+			if (!(Other is SquareMatrix))
+			{
+				return false;
+			}
+
+			return (this.Matrix == ((SquareMatrix)Other).Matrix);
+		}
+		public override int GetHashCode()
+		{
+			int HashCode = 0;
+			for (int Line = 0; Line < 3; ++Line)
+			{
+				for (int Column = 0; Column < 3; ++Column)
+				{
+					HashCode += Matrix[Line, Column];
+				}
+			}
+			return HashCode;
+		}
+		public int CompareTo(object Object)
+		{
+			if (Object == null)
+			{
+				return 1;
+			}
+
+			SquareMatrix OtherMatrix = Object as SquareMatrix;
+			if (OtherMatrix == null)
+			{
+				throw new ArgumentException("Object is not a Matrix");
+			}
+
+			return CompareTo(OtherMatrix);
+		}
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+	}
+}
---
+++ ProgrLab3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ProgrLab3
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      SquareMatrix Matrix1 = new SquareMatrix();
+      Matrix1.FillRandom();
+      SquareMatrix Matrix2 = new SquareMatrix();
+      Matrix2.FillRandom();
+
+      float Result = Matrix1 * Matrix2;
+      Console.WriteLine(Result);
+      Result = Matrix1 - Matrix2;
+      Console.WriteLine(Result);
+
+      ShallowCloneMatrix ClonedMatrix = new ShallowCloneMatrix();
+      ClonedMatrix.Determinant = 2;
+      ShallowCloneMatrix ClonedMatrix2 = (ShallowCloneMatrix)ClonedMatrix.Clone();
+      ClonedMatrix.Determinant = 1;
+      Console.WriteLine(ClonedMatrix.Determinant);
+      Console.WriteLine(ClonedMatrix2.Determinant);
+    }
+  }
+}
